402f7e4abd72395d1ae8225fc4d4cd58fb6ade5d
m-vad <m.vad95@gmail.com>
2022-08-03 11:29:50 +0300

    feat: add testing a component with async data loading from the server

---
+++ react_testing_library/src/users/Users.test.jsx
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react';
+import Users from "./Users";
+import axios from 'axios';
+import React from "react";
+
+jest.mock('axios');
+
+describe('USERS TEST', () => {
+    let response;
+    beforeEach(() => {
+        response = {
+            data: [
+                {
+                    "id": 1,
+                    "name": "Leanne Graham",
+                },
+                {
+                    "id": 2,
+                    "name": "Ervin Howell",
+                },
+                {
+                    "id": 3,
+                    "name": "Clementine Bauch",
+                },
+            ]
+        }
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    })
+
+    test('renders learn react link', async() => {
+        axios.get.mockReturnValue(response);
+        render(<Users />);
+        const users = await screen.findAllByTestId('user-item');
+        expect(users.length).toBe(3);
+        expect(axios.get).toBeCalledTimes(1);
+        screen.debug();
+    });
+})
